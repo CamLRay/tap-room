fb5cf6afcd959c806bf757ac09178bf5dd9bb88e
Sploob <crayiphone@gmail.com>
2022-07-01 11:49:46 -0700

    add basic grid styling

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -3,15 +3,23 @@ import PropTypes from 'prop-types';
 
 function Keg(props){
   const kegImageStyle = {
-    width: '5%'
+    margin:'auto',
+    width: '20%'
+  }
+  const kegComponentStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    justifyContent: 'space-between'
   }
   return(
-    <>
+    <div style={kegComponentStyle}>
+      <p>{props.brand}</p>
       <img style={kegImageStyle} src={props.imgUrl} alt="Logo" />
-      <p>{props.brand} - {props.flavor}</p>
+      <p>{props.flavor} - ABV {props.abv}%</p>
       <p>{props.remainingPints}/{props.maxPints}</p>
       <button>Poured a pint</button>
-    </>
+    </div>
   )
 }
 
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -2,8 +2,16 @@ import React from "react";
 import Keg from "./Keg";
 
 function KegList(props){
+  const kegListContainerStyle = {
+    display: "grid",
+    gridGap: '2em',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+    // flexWrap: 'wrap',
+    width: '100%',
+    justifyContent: 'space-evenly'
+  }
 return(
-  <>
+  <div style={kegListContainerStyle}>
   {props.kegList
     .sort((a,b) => a.brand > b.brand ? 1 : -1)
     .map((keg) =>
@@ -11,13 +19,14 @@ return(
         imgUrl={keg.imgUrl}
         brand={keg.brand}
         flavor={keg.flavor}
+        abv={keg.abv}
         remainingPints={keg.remainingPints}
         maxPints={keg.maxPints}
         id={keg.id}
         key={keg.id}
       />
       )}
-  </>
+  </div>
 )
 }
 
